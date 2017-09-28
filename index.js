9adcdd48c3164434ff37ce8bcd1fd62401996583
Sara Hopper <sara.hopper@gmail.com>
2017-09-28 02:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-// class User {
-//   constructor(name, email) {
-//     this.name = name;
-//     this.email = email;
-//   }
-//
-//   sayHello() {
-//     console.log(`Hello, my name is ${this.name}`);
-//   }
-// }
-//
-//
-// class Teacher extends User {
-//   sayHello(){
-//     super.sayHello()
-//     console.log('hello')
-//   }
-// }
-//
-// tom.sayHello()
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+
+  veto(){
+    return 'No, I must disagree'
+  }
+  
+  approve(){
+    return 'You can do that!'
+  }
+
+  doCharity(){
+    return "I like to help people."
+  }
+
+  releasePressStatement(){
+    return "You will see great things from Scuber."
+  }
+
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+}
